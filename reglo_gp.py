ea43a26e1483aaa53dbd23b62e25bb4a134fab4b
perpetuusScholaris <nazmi.shabnam@gmail.com>
2020-03-30 11:22:41 -0400

    initial files added

---
+++ reglo_gp.py
@@ -0,0 +1,6 @@
+# Shabnam Nazmi.
+# Graduate research assistant at electrical and computer engineering department,
+# North Carolina A&T State University, Greensboro, NC.
+# snazmi@aggies.ncat.edu.
+#
+# ------------------------------------------------------------------------------
