dfd5d612cd2e05a31c88e38ce0a7e3ac7767dcd2
perpetuusScholaris <nazmi.shabnam@gmail.com>
2020-05-27 20:51:13 -0400

    continuous update

--- config.py
+++ config.py
@@ -16,5 +16,20 @@ DATA_DIR = "D:\Datasets"
 DATA_HEADER = "emotions"
 NO_FEATURES = 72
 
+MAX_ITERATION = 10000
+MAX_CLASSIFIER = 100
+PROB_HASH = 0.5
+DO_SUBSUMPTION = False
+
+THETA = 0.5
+RANK_CUT = 1
+
+# ------------------------------------------------------------------------------
+INIT_FITNESS = 0.01
+NU = 15
+THETA_SUB = 10
+LOSS_SUB = 0.1
+BETA = 0.1
+
 
 
