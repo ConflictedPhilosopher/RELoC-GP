31ec91ab6cfee5cd41a43c156415ba4082e4f52c
perpetuusScholaris <nazmi.shabnam@gmail.com>
2020-08-12 15:40:05 -0400

    debugged

--- reporting.py
+++ reporting.py
@@ -26,21 +26,24 @@ class Reporting:
             raise
 
         stat_file.write("Model characterization:\n")
-        stat_file.write("Macro pop size: " + str(pop.__len__()) + "\tMicro pop size: " + str(pop.micro_pop_size) +
-                        "\tAvg generality:" + str("%.3f" % pop.ave_generality) + "\n")
+        stat_file.write("Macro pop size: " + str(pop.popset.__len__()) + "\tMicro pop size: " + str(pop.micro_pop_size) +
+                        "\tAvg generality:" + str("%.3f" % pop.ave_generality) + "\n\n")
 
         stat_file.write("Run time (min):\n")
         stat_file.write(timer.get_timer_report())
+        stat_file.write("\n")
 
-        stat_file.write("Training instance coverage: " + str("%.4f" % train_coverage))
-        stat_file.write("Test instance coverage: " + str("%.4f" % test_coverage))
+        stat_file.write("Training instance coverage: " + str("%.4f" % train_coverage) + "\n")
+        stat_file.write("Test instance coverage: " + str("%.4f" % test_coverage) + "\n\n")
 
         stat_file.write("Training performance:\n")
         [stat_file.write(key + "\t") for key in train_eval.keys()]
-        [stat_file.write(str("%.4f\t" % value)) for value in train_eval.values()]
         stat_file.write('\n')
+        [stat_file.write(str("%.4f\t" % value)) for value in train_eval.values()]
+        stat_file.write('\n\n')
         stat_file.write("Test performance:\n")
         [stat_file.write(key + "\t") for key in test_eval.keys()]
+        stat_file.write('\n')
         [stat_file.write(str("%.4f\t" % value)) for value in test_eval.values()]
         stat_file.close()
 
@@ -58,7 +61,7 @@ class Reporting:
         method = ClassifierMethods(dtypes)
         header = ",".join(['f'+str(i) for i in range(NO_FEATURES)])
         header += ", prediction, fitness, hloss, correct_count, numerosity, match_count" \
-                  "avg_match_set, init_time, ga_time"
+                  "avg_match_set, init_time, ga_time \n"
         model_file.write(header)
         [model_file.write(method.classifier_print(cl)) for cl in pop]
         model_file.close()
