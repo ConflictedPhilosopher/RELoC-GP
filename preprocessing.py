a134c1d5e0e543f6d3958cca56dbdcf6aa1e5a2e
Shabnam <nazmi.shabnam@gmail.com>
2020-09-23 12:13:59 -0400

    sample id and label-ref added

--- preprocessing.py
+++ preprocessing.py
@@ -15,28 +15,26 @@ from config import *
 class Preprocessing:
     def __init__(self):
         self.label_count = 0
-        self.label_dict = {}
-        self.distinct_lp = 0
+        self.label_ref = dict()
+        self.distinct_lp_count = 0
         self.unseen_test_lp = []
-        self.unseen_test_labels = {}
-        self.imbalance_lp = 0
+        self.unseen_test_labels = set()
         self.card = 0.0
         self.density = 0.0
-        self.class_dict = []
+        self.class_count = dict()
+        self.imbalance_lp = 0
         self.imbalance_mean = 0.0
         self.cvir = 0.0
         self.attribute_info = []
         self.dtypes = []
+        self.id = False
         self.data_train_list = []
         self.data_test_list = []
-        self.data_train_count = 0
-        self.data_test_count = 0
-        self.data_complete_count = 0
         self.data_train_folds = []
         self.data_valid_folds = []
         self.default_split = 0.7
 
-    def main(self,  train_test, cv, complete):
+    def main(self, train_test, cv, complete):
         data_path = os.path.join(DATA_DIR, DATA_HEADER, DATA_HEADER + ".csv")
         train_data_path = os.path.join(DATA_DIR, DATA_HEADER, DATA_HEADER + "_train.csv")
         test_data_path = os.path.join(DATA_DIR, DATA_HEADER, DATA_HEADER + "_test.csv")
@@ -46,8 +44,6 @@ class Preprocessing:
         if train_test:
             data_train = self.load_data(train_data_path)
             data_test = self.load_data(test_data_path)
-            self.data_train_count = len(data_train)
-            self.data_test_count = len(data_test)
             data_complete = pd.concat([data_train, data_test])
 
             self.data_train_list = self.format_data(data_train)
@@ -62,23 +58,27 @@ class Preprocessing:
         else:
             print('Error: No data file specified')
             return
-        self.data_complete_count = data_complete.__len__()
         self.characterize_features(data_complete)
         self.characterize_labels(data_complete)
         if GET_MLD_PROP:
             self.multilabel_properties(data_complete)
 
-# load data (.csv)
+    # load data (.csv)
     def load_data(self, path):
         try:
             drop_index = []
             data = pd.read_csv(path)
             label_set_list = []
+            try:
+                data.set_index('ID', drop=True, inplace=True)
+                self.id = True
+            except KeyError:
+                pass
             for idx, row in data.iterrows():
                 label = [int(l) for l in row[NO_FEATURES:]]
                 label_set = set([idx for idx, val in enumerate(label) if val == 1])
                 label_set_list.append(label_set)
-                if label_set.__len__() < 1:
+                if label_set.__len__() < 1:  # removes samples with no label
                     drop_index.append(idx)
             data['labelset'] = label_set_list
             data.drop(drop_index, axis=0, inplace=True)
@@ -86,9 +86,9 @@ class Preprocessing:
         except FileNotFoundError as fnferror:
             print(fnferror)
 
-# feature selection
+    # feature selection
 
-# characterize features
+    # characterize features
     def characterize_features(self, data_complete):
         for dtype in data_complete.iloc[:, :NO_FEATURES].dtypes:
             if dtype == "float64":
@@ -101,9 +101,9 @@ class Preprocessing:
                 self.attribute_info.append(0)
             elif dtype == "float64":
                 self.attribute_info.append([data_complete.iloc[:, it].min(),
-                                           data_complete.iloc[:, it].max()])
+                                            data_complete.iloc[:, it].max()])
 
-# characterize classes
+    # characterize classes
     def characterize_labels(self, data_complete):
         self.label_count = NO_LABELS
         lp_dict = {}
@@ -112,59 +112,56 @@ class Preprocessing:
                 lp_dict[str(label)] += 1
             else:
                 lp_dict[str(label)] = 1
-        self.distinct_lp = lp_dict.__len__()
-        self.imbalance_lp = max(lp_dict.values()) \
-                            / min(lp_dict.values())
+        self.distinct_lp_count = lp_dict.__len__()
+        self.imbalance_lp = max(lp_dict.values()) / min(lp_dict.values())
+        self.label_ref = {k: v for k, v in enumerate(data_complete.columns[NO_FEATURES:-1])}
 
-        label_list = []
-        if self.data_train_list:
-            for row in self.data_train_list:
-                if row[-1] in label_list:
-                    pass
-                else:
-                    label_list.append(row[-1])
-            for row in self.data_test_list:
-                if row[-1] in label_list:
-                    pass
-                else:
-                    self.unseen_test_lp.append(row[-1])
-
-# ِ multi-label properties
+    # ِ multi-label properties
     def multilabel_properties(self, data_complete):
         count = sum([len(label) for label in data_complete['labelset']])
-        self.card = count / self.data_complete_count
-        self.density = self.card / self.label_count
+        self.card = count / data_complete.__len__()
+        self.density = self.card / NO_LABELS
         counts = [data_complete[classs].sum() for classs in data_complete.columns[NO_FEATURES:-1]]
-        self.class_dict = dict(zip(list(data_complete.columns)[NO_FEATURES:-1], counts))
-        class_pi = [val / self.data_complete_count for val in list(self.class_dict.values())]
+        self.class_count = dict(zip(list(data_complete.columns)[NO_FEATURES:-1], counts))
+        class_pi = [val / data_complete.__len__() for val in list(self.class_count.values())]
         imbalance_label = [max(class_pi) / val for val in class_pi]
-        self.imbalance_mean = sum(imbalance_label) / self.label_count
+        self.imbalance_mean = sum(imbalance_label) / NO_LABELS
         temp = [(val - self.imbalance_mean) ** 2 for val in imbalance_label]
         imbalance_label_sigma = sqrt(sum(temp) / (self.label_count - 1))
         self.cvir = imbalance_label_sigma / self.imbalance_mean
 
-        test_labels = set.union(*[row[-1] for row in self.data_test_list])
-        self.unseen_test_labels = test_labels.difference(set.union(*[row[-1] for row in self.data_train_list]))
+        label_list = []
+        if self.data_train_list:
+            for row in self.data_train_list:
+                if row[1] in label_list:
+                    pass
+                else:
+                    label_list.append(row[1])
+            for row in self.data_test_list:
+                if row[1] in label_list:
+                    pass
+                else:
+                    self.unseen_test_lp.append(row[1])
+        test_labels = set.union(*[row[1] for row in self.data_test_list])
+        self.unseen_test_labels = test_labels.difference(set.union(*[row[1] for row in self.data_train_list]))
         self.print_mldp()
 
     def print_mldp(self):
         print('Multi-label stats:')
-        print('Training/Test samples: {} / {}'.format(self.data_train_count, self.data_test_count))
+        print('Training/Test samples: {} / {}'.format(self.data_train_list.__len__(), self.data_test_list.__len__()))
         print('Lcard: %.4f' % self.card)
         print('Ldens: %.4f' % self.density)
         print('Unseen test LP: %d' % self.unseen_test_lp.__len__())
         if self.unseen_test_labels:
             print('Unseen test labels: ', self.unseen_test_labels)
 
-# train-test split
+    # train-test split
     def train_test_split(self, data_complete):
         data_train, data_test = train_test_split(data_complete,
-                                test_size=1-self.default_split, random_state=SEED_NUMBER)
-        self.data_train_count = len(data_train)
-        self.data_test_count = len(data_test)
+                                                 test_size=1 - self.default_split, random_state=SEED_NUMBER)
         return [data_train, data_test]
 
-# cross-validation data
+    # cross-validation data
     def cross_validation_folds(self, data_folds):
         k_fold = len(data_folds)
         fold_list = [self.load_data(name) for name in data_folds]
@@ -176,16 +173,18 @@ class Preprocessing:
             valid_fold_list = self.format_data(valid_fold)
             self.data_train_folds.append(train_fold_list)
             self.data_valid_folds.append(valid_fold_list)
-        data_complete = pd.concat(fold_list)
-        self.data_complete_count = data_complete.__len__()
-        return data_complete
+        return pd.concat(fold_list)
 
-# format data
+    # format data
     def format_data(self, data):
         data_list = []
         data.sample(frac=1.0, random_state=SEED_NUMBER)
-        for idx, row in data.iterrows():
-            data_list.append([list(row[:NO_FEATURES]), row[-1]])
+        if self.id:
+            for idx, row in data.iterrows():
+                data_list.append([list(row[:NO_FEATURES]), row[-1], idx])
+        else:
+            for idx, row in data.iterrows():
+                data_list.append([list(row[:NO_FEATURES]), row[-1]])
         return data_list
 
 
@@ -193,7 +192,7 @@ if __name__ == "__main__":
     data_path0 = os.path.join(DATA_DIR, DATA_HEADER, DATA_HEADER + ".csv")
     train_data_path0 = os.path.join(DATA_DIR, DATA_HEADER, DATA_HEADER + "_train.csv")
     test_data_path0 = os.path.join(DATA_DIR, DATA_HEADER, DATA_HEADER + "_test.csv")
-    fold_path0 = [os.path.join(DATA_DIR, DATA_HEADER, DATA_HEADER + "_fold_" + str(i+1) + ".csv") for i in range(5)]
+    fold_path0 = [os.path.join(DATA_DIR, DATA_HEADER, DATA_HEADER + "_fold_" + str(i + 1) + ".csv") for i in range(5)]
 
     preprocessing = Preprocessing()
     preprocessing.main(0, 0, 1)
