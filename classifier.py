babd810275bb2ccd6f9abb52aff151d5e494f0bb
Shabnam <nazmi.shabnam@gmail.com>
2020-11-03 16:57:22 -0500

    new method added, var name changed, new var added

--- classifier.py
+++ classifier.py
@@ -20,6 +20,7 @@ def build_match(x, att_info, dtype, random_func):
     else:
         print("attribute type unidentified!")
 
+
 class Classifier:
     def __init__(self):
         self.specified_atts = []
@@ -30,7 +31,8 @@ class Classifier:
         self.match_count = 0
         self.loss = 0.0
         self.fscore = 0.0
-        self.label_based = {}
+        self.label_based_tp = {}
+        self.est_label_precision = {}
         self.fitness = INIT_FITNESS
         self.ave_matchset_size = 0
         self.init_time = 0
@@ -48,7 +50,8 @@ class Classifier:
                     self.specified_atts.append(ref)
                     self.condition.append(build_match(x, attribute_info[ref], dtypes[ref], random_func))
                     og = False
-        self.label_based = {k: 0 for k in self.prediction}
+        self.label_based_tp = {k: 0 for k in self.prediction}
+        self.est_label_precision = {k: 0.0 for k in self.prediction}
 
     def classifier_copy(self, classifier_old, it):
         self.specified_atts = deepcopy(classifier_old.specified_atts)
@@ -60,19 +63,20 @@ class Classifier:
         self.ga_time = it
         self.fitness = classifier_old.fitness
         self.loss = classifier_old.loss
-        self.label_based = {k: 0 for k in self.prediction}
+        self.label_based_tp = {k: 0 for k in self.prediction}
+        self.est_label_precision = {k: 0.0 for k in self.prediction}
 
     def classifier_reboot(self, classifier_info, dtypes):
         classifier_info = classifier_info.to_list()
         condition = classifier_info[:NO_FEATURES]
 
-        def update_cond(ref, att_val):
-            if dtypes[ref] == 1:
-                self.specified_atts.append(ref)
-                self.condition.append([float(x) for x in att_val.split(";")])
+        def update_cond(ref0, att_val0):
+            if dtypes[ref0] == 1:
+                self.specified_atts.append(ref0)
+                self.condition.append([float(x) for x in att_val0.split(";")])
             else:
-                self.specified_atts.append(ref)
-                self.condition.append(int(att_val))
+                self.specified_atts.append(ref0)
+                self.condition.append(int(att_val0))
 
         for ref, att_val in enumerate(condition):
             if att_val == '#' or att_val == ' #':
@@ -82,9 +86,11 @@ class Classifier:
 
         self.prediction = set(int(n) for n in classifier_info[NO_FEATURES].split(";"))
         label_precisions = classifier_info[NO_FEATURES + 1]
-        self.label_based = {int(kv.split(":")[0]): float(kv.split(":")[1]) for kv in label_precisions.split(";")}
-        self.fitness, self.loss, self.numerosity, self.match_count, self.ave_matchset_size, \
-        self.init_time, self.ga_time = classifier_info[NO_FEATURES + 2:]
+        self.label_based_tp = {int(kv.split(":")[0]): float(kv.split(":")[1]) for kv in label_precisions.split(";")}
+        self.fitness, self.loss, self.numerosity, self.match_count, self.ave_matchset_size, self.init_time, self.ga_time\
+            = classifier_info[NO_FEATURES + 2:]
+
+        # TODO parent prediction and estimated label precision to be added
 
     def update_numerosity(self, num):
         self.numerosity += num
@@ -107,25 +113,35 @@ class Classifier:
             self.loss += (self.prediction.symmetric_difference(target).__len__() /
                           (float(self.prediction.__len__() + target.__len__())))
 
-        # update label_based(target)
+        # update label_based_tp(target)
         for l in self.prediction.intersection(target):
-            self.label_based[l] += 1
+            self.label_based_tp[l] += 1
 
         # update f-score(target)
-        self.fscore += (2 * self.prediction.intersection(target).__len__() /
-                        (self.prediction.__len__() + target.__len__()))
+        # self.fscore += (2 * self.prediction.intersection(target).__len__() /
+        #                 (self.prediction.__len__() + target.__len__()))
 
         # update_fitness()
-        self.fitness = max((1 - self.loss/self.match_count)**NU, INIT_FITNESS)
+        self.fitness = max((1 - self.loss / self.match_count) ** NU, INIT_FITNESS)
         # weighted_accuracy_sum = sum([acc/(self.match_count*class_ratio[label]) for
-        #                              label, acc in self.label_based.items()]) / \
+        #                              label, acc in self.label_based_tp.items()]) / \
         #                             sum([1/class_ratio[label] for label in self.prediction])
-        # accuracy_sum = sum([acc / self.match_count for acc in self.label_based.values()]) \
+        # accuracy_sum = sum([acc / self.match_count for acc in self.label_based_tp.values()]) \
         #                / float(self.prediction.__len__())
         # self.fitness = max(((accuracy_sum + self.fscore/self.match_count) / 2) ** NU, INIT_FITNESS)
 
     def set_fitness(self, fitness):
         self.fitness = fitness
+
+    def estimate_label_based(self, conditional):
+        self.est_label_precision = {k: v / self.match_count for k, v in self.label_based_tp.items()}
+        if self.prediction.__len__() > 1:
+            estimate = dict.fromkeys(list(self.label_based_tp.keys()))
+            for l, tp in self.label_based_tp.items():
+                q = conditional[l, list(self.label_based_tp.keys())].sum() - 1
+                if q > 0:
+                    estimate[l] = tp / self.match_count * (1 / q)
+            self.est_label_precision = estimate
 
 
 if __name__ == "__main__":
